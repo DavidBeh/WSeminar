0ef6a1f48b916d856f95e5f4a86f765c80e75f0b
davidbeh <david.behres@gmail.com>
2022-10-05 14:43:52 +0200

    console output

---
+++ dotnet/WSeminar/WSeminar.V2G.Simulation.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using Deedle;
+
+Console.WriteLine("Hello, World!");
+
+var erzeugungStream = File.OpenRead("Realisierte_Erzeugung_202209250000_202210052359.csv");
+var culture = CultureInfo.CreateSpecificCulture("de-DE");
+var erzeugungRoh = Frame.ReadCsv(erzeugungStream, true, separators:";");
+var indexed = erzeugungRoh.IndexRowsUsing(series =>
+{
+    var datum = DateOnly.Parse(series.GetAtAs<string>(0), culture);
+    var zeit = TimeOnly.Parse(series.GetAtAs<string>(1), culture);
+    return new DateTime(datum.Year, datum.Month, datum.Day, zeit.Hour, zeit.Minute, zeit.Second);
+
+
+});
+
+indexed.Print();
--- dotnet/WSeminar/WSeminar.V2G.Simulator.Server/Program.cs
+++ dotnet/WSeminar/WSeminar.V2G.Simulator.Server/Program.cs
@@ -11,16 +11,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-var cachePolicy = Policy.CacheAsync(null,null);
-
-builder.Services.AddHttpClient<SmardClient>().AddPolicyHandler().AddTransientHttpErrorPolicy(policyBuilder =>
-    policyBuilder.WaitAndRetryAsync(new[]
-    {
-        TimeSpan.FromSeconds(1),
-        TimeSpan.FromSeconds(5),
-        TimeSpan.FromSeconds(10)
-    }));
-
+builder.Services.AddHttpClient<SmardClient>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
